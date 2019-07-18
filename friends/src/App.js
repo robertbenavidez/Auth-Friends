0d49730e80a60b0ca0ac64a650835fc33d68ef42
Robert Benavidez <murabitbenavidez@gmail.com>
2019-07-18 17:30:17 -0400

    Initial commit  Setup

---
+++ friends/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I'm ready to code</h1>
+    </div>
+  );
+}
+
+export default App;
